1de6d1b830710e0786713222438b7fb43134c9ff
splakhotniuk <plspls@bigmir.net>
2021-02-25 00:02:22 +0200

    Initial commit

---
+++ src/main/java/stud/springdb/springpsql/SpringpsqlApplication.java
@@ -0,0 +1,13 @@
+package stud.springdb.springpsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stud/springdb/springpsql/SpringpsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.springdb.springpsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
