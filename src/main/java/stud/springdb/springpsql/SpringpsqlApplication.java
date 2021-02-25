38f9b8dab2ddb24ed9e057da55f065a5ba9945f1
splakhotniuk <plspls@bigmir.net>
2021-02-25 11:44:11 +0200

    made appl properties for Postgres

--- src/main/java/stud/springdb/springpsql/SpringpsqlApplication.java
+++ src/main/java/stud/springdb/springpsql/SpringpsqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringpsqlApplication {
 
 	public static void main(String[] args) {
+		// System.out.println("STARTED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		SpringApplication.run(SpringpsqlApplication.class, args);
 	}
 
